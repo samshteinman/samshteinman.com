b8abd8946716a3c29652560c4f2848ec82d0007b
samshteinman <samshteinman@gmail.com>
2021-01-09 16:40:10 -0500

    update to angular 11

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { AvatarComponent } from './avatar/avatar.component';
 import { BrandComponent } from './brand/brand.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
--- src/app/my-select/my-select.component.spec.ts
+++ src/app/my-select/my-select.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { MySelectComponent } from './my-select.component';
 
@@ -6,7 +6,7 @@ describe('MySelectComponent', () => {
   let component: MySelectComponent;
   let fixture: ComponentFixture<MySelectComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MySelectComponent ]
     })
