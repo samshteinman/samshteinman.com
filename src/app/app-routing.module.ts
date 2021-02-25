808b2a14c48a7aa6800ba6890197481d7d252213
samshteinman <samshteinman@gmail.com>
2021-02-24 21:33:49 -0500

    added music and blog page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MyMusicComponent } from './pages/my-music/my-music.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: 'music', component: MyMusicComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: '', component: HomeComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AvatarComponent } from  './avatar/avatar.component';
 import { BrandComponent } from  './brand/brand.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MyMusicComponent } from './pages/my-music/my-music.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
-import { HomeComponent } from './pages/home/home.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { BlogComponent } from './pages/blog/blog.component';
+
+const MATERIAL = [
+  MatIconModule
+]
 
 @NgModule({
   declarations: [
@@ -17,14 +27,17 @@ import { HomeComponent } from './pages/home/home.component';
     AvatarComponent,
     BrandComponent,
     HeaderNavComponent,
-    HomeComponent
+    HomeComponent,
+    MyMusicComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
     BrowserAnimationsModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ...MATERIAL
   ],
   providers: [],
   bootstrap: [AppComponent]
